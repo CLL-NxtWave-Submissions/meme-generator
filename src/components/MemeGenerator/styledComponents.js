97f2d5e5ce9747601d571117280eecea9011fd67
Surya T N <suryatej.nim@yahoo.com>
2022-11-10 09:56:32 +0000

    Added partial styling for JSX elements in MemeGenerator component, using styled-components package

--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -1,1 +1,12 @@
-// Style your components here
+import styled from 'styled-components'
+
+export const MemeHeader = styled.h1`
+  color: #35469c;
+  font-size: 2rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+`
+export const MemeGeneratorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
