7824d913b455e69d520f22eb4156ac72cffa108a
Surya T N <suryatej.nim@yahoo.com>
2022-11-10 12:17:46 +0000

    Added styling for more JSX elements in MemeGenerator component, apart from the meme generator data input container and its elements

--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
 
+export const MemeGeneratorBgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5rem;
+  font-family: 'Roboto';
+  margin: 0;
+`
+
 export const MemeHeader = styled.h1`
   color: #35469c;
   font-size: 2rem;
@@ -9,4 +19,37 @@ export const MemeHeader = styled.h1`
 export const MemeGeneratorContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  margin: 0;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`
+
+export const GeneratedMemeContainer = styled.div`
+  margin: 0;
+  width: 100%;
+  background-image: url(${props => props.src});
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+
+  @media (min-width: 768px) {
+    width: 50%;
+    order: 1;
+  }
+`
+
+export const GeneratedMemeText = styled.p`
+  color: #ffffff;
+  font-size: ${props => props.fontSize};
+  font-weight: 550;
+  text-align: center;
+  line-height: 1.5rem;
+  padding: 1rem;
 `
