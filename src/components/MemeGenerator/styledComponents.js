fa87a369f828a2b36217e016b38205762f238805
Surya T N <suryatej.nim@yahoo.com>
2022-11-11 06:00:03 +0000

    Added CSS styling for all JSX elements in MemeGenerator component, through styled components

--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -40,7 +40,8 @@ export const GeneratedMemeContainer = styled.div`
   height: 100%;
 
   @media (min-width: 768px) {
-    width: 50%;
+    width: 40%;
+    min-width: 40%;
     order: 1;
   }
 `
@@ -52,4 +53,48 @@ export const GeneratedMemeText = styled.p`
   text-align: center;
   line-height: 1.5rem;
   padding: 1rem;
+`
+
+export const MemeConfigurationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    width: 40%;
+  }
+`
+export const MemeConfigurationInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 1rem 0;
+  padding: 0;
+  width: 100%;
+  color: #d7dfe9;
+  font-size: 1rem;
+`
+
+export const MemeConfigurationInputLabel = styled.label`
+  margin-bottom: 0.5rem;
+`
+
+export const MemeConfigurationInput = styled.input`
+  type="text";
+  outline: none;
+  border: 0.1rem solid #d7dfe9;
+  border-radius: 0.4rem;
+  padding: 0.5rem 1rem;
+`
+
+export const MemeGenerateButton = styled.button`
+  border: none;
+  outline: none;
+  background-color: #0b69ff;
+  color: #ffffff;
+  border-radius: 0.4rem;
+  padding: 1rem 2.5rem;
 `
